7589b7cc7fc5dd92c4ee0c4f1f28a12d8b22f4cc
vwf <vwff@icloud.com>
2021-07-02 13:34:02 +0500

    Initial Commit

---
+++ LoginScreenObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LoginScreenObjC
+//
+//  Created by FARIT GATIATULLIN on 02.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LoginScreenObjC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LoginScreenObjC
+//
+//  Created by FARIT GATIATULLIN on 02.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
